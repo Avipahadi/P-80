7f4ee96b092ea0ceed15f9a9a49d064bf085ace1
Arnav Sharma <68996768+Avipahadi@users.noreply.github.com>
2021-01-02 14:46:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function get_p1() {
     for (var i = 1; i <= 6; i++) {
         inputs_1.push(document.getElementById("input_p1_" + i).value);
     }
-    document.getElementById("p_1").innerHTML = inputs_1.join(". ");
+    document.getElementById("p_1").innerHTML = inputs_1.join(" ");
 }
 
 function get_p2() {
@@ -11,5 +11,5 @@ function get_p2() {
     for (var x = 1; x <= 6; x++) {
         inputs_2.push(document.getElementById("input_p2_" + x).value);
     }
-    document.getElementById("p_2").innerHTML = inputs_2.join(". ");
+    document.getElementById("p_2").innerHTML = inputs_2.join(" ");
 }
